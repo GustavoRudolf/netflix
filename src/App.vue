a868fdcec763244bc59de2643bb43b3fdc1b4f8a
Gustavo Rudolf <gustavorudolfifc@gmail.com>
2022-10-11 09:28:13 -0300

    salvo

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+import CarouselComp from "@/components/CarouselComp.vue";
+</script>
+<template>
+  <div class="nome text-center">
+    <h1>GR FLIX</h1>
+  </div>
+
+  <NavBar />
+  <CarouselComp />
+
+  <div class="container text-light" id="filmes">
+    <h2>FILMES</h2>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="@/assets/img/homem aranha.jpg" class="img-fluid" alt="..." />
+      </div>
+      <div class="col-md-3">
+        <img src="@/assets/img/transformers.jpg" class="img-fluid" alt="..." />
+      </div>
+      <div class="col-md-3">
+        <img src="@/assets/img/birdbox.jpg" class="img-fluid" alt="..." />
+      </div>
+      <div class="col-md-3">
+        <img
+          src="@/assets/img/velozes e furiosos.jpg"
+          class="img-fluid"
+          alt="..."
+          width="280"
+        />
+      </div>
+    </div>
+  </div>
+  <br />
+  <div class="container text-light" id="series">
+    <h2>SÉRIES</h2>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <img
+          src="@/assets/img/twd.jpg"
+          class="img-fluid"
+          alt="..."
+          width="1000"
+        />
+      </div>
+      <div class="col-md-3">
+        <img src="@/assets/img/GOT.jpg" class="img-fluid" alt="..." />
+      </div>
+    </div>
+    <div class="col-md-3">
+      <img src="@/assets/img/the 100.jfif" class="img-fluid" alt="..." />
+    </div>
+    <div class="col-md-3">
+      <img
+        src="@/assets/img/perdidos no espaço.jpg"
+        class="img-fluid"
+        alt="..."
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light cor1">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">GR FLIX</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul
+          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+          style="--bs-scroll-height: 100px"
+        >
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"
+              >PÁGINA PRINCIPAL</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FILMES</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarScrollingDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="true"
+            >
+              OPÇÕES
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="#filmes">FILMES</a></li>
+              <li><a class="dropdown-item" href="#series">SÉRIES</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">ANIMES</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">PLANOS</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Pesquisar
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
